bccff085da0cbaff8f988cbc6f195efb93669f4f
Srikar Vanavasam <srikar.vanavasam@gmail.com>
2023-09-19 17:37:14 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,3 +10,7 @@ wget -P ~/.local/bin/ https://github.com/neovim/neovim/releases/download/v0.9.1/
 chmod u+x ~/.local/bin/nvim.appimage
 ln -s ~/.local/bin/nvim.appimage ~/.local/bin/nvim
 git clone https://github.com/SrikarVanavasam/LazyVim.config.git ~/.config/nvim
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x Miniconda3-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda
+./miniconda/bin/conda init zsh
