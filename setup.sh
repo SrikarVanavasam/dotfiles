2dcbf3fe0d9eafc7fe7463f14b3a06bae983d9b6
Srikar Vanavasam <srikar.vanavasam@gmail.com>
2023-09-19 17:13:57 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+rm .zshrc; curl -L https://raw.githubusercontent.com/SrikarVanavasam/dotfiles/main/.zshrc > .zshrc
 mkdir -p ~/.local/bin
 wget -P ~/.local/bin/ https://github.com/neovim/neovim/releases/download/v0.9.1/nvim.appimage
 chmod u+x ~/.local/bin/nvim.appimage
