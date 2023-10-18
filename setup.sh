7bfa27a73ad05f44dc876f44579b89b1a140a911
Srikar Vanavasam <srikar.vanavasam@gmail.com>
2023-10-18 16:46:03 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,4 +14,4 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-rm .zshrc; curl -L https://raw.githubusercontent.com/SrikarVanavasam/dotfiles/main/.zshrc > .zshrcls   
+rm .zshrc; curl -L https://raw.githubusercontent.com/SrikarVanavasam/dotfiles/main/.zshrc > .zshrc
