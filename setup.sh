7aeb33b64b89a830741d5e40c437e92e31e983f6
Srikar Vanavasam <srikar.vanavasam@gmail.com>
2023-12-02 15:47:56 -0600

    switch to fish

--- setup.sh
+++ setup.sh
@@ -3,15 +3,11 @@ wget -P ~/.local/bin/ https://github.com/neovim/neovim/releases/download/v0.9.1/
 chmod u+x ~/.local/bin/nvim.appimage
 ln -s ~/.local/bin/nvim.appimage ~/.local/bin/nvim
 git clone https://github.com/SrikarVanavasam/LazyVim.config.git ~/.config/nvim
+sudo apt-get install fish
+chsh -s $(which fish)
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x Miniconda3-latest-Linux-x86_64.sh
 ./Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda
-./miniconda/bin/conda init zsh
+./miniconda/bin/conda init fish
 rm Miniconda3-latest-Linux-x86_64.sh
-sudo apt-get install zsh
-chsh -s $(which zsh)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-rm .zshrc; curl -L https://raw.githubusercontent.com/SrikarVanavasam/dotfiles/main/.zshrc > .zshrc
