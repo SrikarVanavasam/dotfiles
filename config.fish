700c8fe79599d8eec365a2bbf0f69f5cdd91e2a5
Srikar Vanavasam <srikar.vanavasam@gmail.com>
2024-07-16 11:36:27 -0700

    add binding for accepting auto suggestion

--- config.fish
+++ config.fish
@@ -42,6 +42,7 @@ set -g pure_color_mute green
 fish_vi_key_bindings
 bind --mode insert --sets-mode default jk repaint
 bind --mode insert --sets-mode default jj repaint
+bind -M insert \ce accept-autosuggestion
 
 # environment variables
 fish_add_path ~/.local/bin
