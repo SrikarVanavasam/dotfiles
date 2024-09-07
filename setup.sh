67efd09f8605a9ddaa122072a6507fc0542a3379
Srikar Vanavasam <srikar.vanavasam@gmail.com>
2024-09-07 18:07:48 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,7 +11,6 @@ wget -P ~/.local/bin/ https://github.com/neovim/neovim/releases/download/v0.10.1
 chmod u+x ~/.local/bin/nvim.appimage
 ln -s ~/.local/bin/nvim.appimage ~/.local/bin/nvim
 git clone git@github.com:SrikarVanavasam/LazyVim.config.git ~/.config/nvim
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 curl -L https://raw.githubusercontent.com/SrikarVanavasam/dotfiles/main/config.fish > .config/fish/config.fish
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x Miniconda3-latest-Linux-x86_64.sh
